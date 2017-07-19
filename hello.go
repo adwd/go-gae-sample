9fd8f47e04b59f053bb3b7e1e8e549ad899cbb50
西田 雅博 <masahiro.nishida@bizreach.co.jp>
2017-07-20 07:59:25 +0900

    add index template

--- hello.go
+++ hello.go
@@ -5,13 +5,23 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"html/template"
 )
 
 func init() {
 	r := mux.NewRouter()
-	r.HandleFunc("/user/{name}", hello)
+	r.HandleFunc("/users/{name}", hello)
+	r.HandleFunc("/", index)
 	http.Handle("/", r)
 }
+
+func index(w http.ResponseWriter, r *http.Request) {
+	if err := indexTemplate.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+var indexTemplate = template.Must(template.ParseFiles("templates/index.html"))
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	u := mux.Vars(r)["name"]
